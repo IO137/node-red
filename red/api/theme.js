50a268d61631b19fca5206a8bfca5091f645d442
graphyr <hello@graphyr.com>
2016-04-09 16:24:30 +1200

    styling

--- red/api/theme.js
+++ red/api/theme.js
@@ -22,11 +22,11 @@ var clone = require("clone");
 
 var defaultContext = {
     page: {
-        title: "Node-RED",
+        title: "Litae",
         favicon: "favicon.ico"
     },
     header: {
-        title: "Node-RED",
+        title: "Litae",
         image: "red/images/node-red.png"
     },
     asset: {
