e385d883c9d772cb3e06a47c2ecb619f07a92603
graphyr <hello@graphyr.com>
2016-05-02 20:33:13 +1200

    switch dirs

--- kobble-node-red.js
+++ kobble-node-red.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var fs = require('fs');
+var _ = require('lodash');
+var path = require('path');
+var es2015 = path.join(__dirname, "node_modules", "babel-preset-es2015");
+var stage0 = path.join(__dirname, "node_modules", "babel-preset-stage-0");
+//console.log("preset: " + es2015 + " " + stage0);
+
 // Babel
 console.log("status:Starting kobble instance...");
 require('babel-core/register')({
-  presets: [ 'es2015', 'stage-0' ],
-  only: [/.*(node-red-contrib|kobble).*/]
+  presets: [ es2015, stage0 ],
+  only: [/.*(kobble).*/]
 });
 require('babel-polyfill');
 
@@ -16,23 +23,32 @@ var RED = require("./red/red.js");
 var port = process.argv.length > 2 ? parseInt(process.argv[2]) : 1880;
 var ff = process.argv.length > 3 ? process.argv[3] : "";
 var kobbleDir = process.argv.length > 4 ? process.argv[4] : "";
-var nodesDir = process.argv.length > 5 ? process.argv[5] : "";
-console.log("port: " + port + " " + " kob:" + ff + " kobbleDir:" + kobbleDir);
+var userDir = process.argv.length > 5 ? process.argv[5] : kobbleDir;
+var userSettingsFile = path.join(userDir, "settings.json");
+var userSettings = {};
+try {
+    fs.accessSync(userSettingsFile, fs.F_OK);
+    var userSettingsStr = fs.readFileSync(userSettingsFile, "utf8");
+    userSettings = JSON.parse(userSettingsStr);
+} catch (e) {
+}
+
+console.log("port: " + port + " " + " kob:" + ff + " kobbleDir:" + kobbleDir + " userDir: " + userDir);
 
 var xapp = express();
 xapp.use("/",express.static("public"));
 var server = http.createServer(xapp);
-var settings = {
+var kobSettings = {
   flowsFullPath: ff,
   uiPort: port,
-  nodesDir: nodesDir,
+  verbose: true,
   noBackup: true,
   kobbleDir: kobbleDir,
   httpAdminRoot:"/",
   httpNodeRoot: "/",
-  userDir:kobbleDir,
-  functionGlobalContext: { }
+  userDir:userDir
 };
+var settings = _.merge(userSettings, kobSettings);
 RED.init(server,settings);
 xapp.use(settings.httpAdminRoot,RED.httpAdmin);
 xapp.use(settings.httpNodeRoot,RED.httpNode);
